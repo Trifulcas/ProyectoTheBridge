d45cb331c3e133867d7f50f53e4d426a1c9b8d05
Trifulcas <157748610+Trifulcas@users.noreply.github.com>
2024-01-25 21:30:05 +0100

    Update ProyectofinalApplication.java
    Añadir scan de componentes

--- Proyectofinal/src/main/java/com/trifulcas/Proyectofinal/ProyectofinalApplication.java
+++ Proyectofinal/src/main/java/com/trifulcas/Proyectofinal/ProyectofinalApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan("com.trifulcas")
 public class ProyectofinalApplication {
 
 	public static void main(String[] args) {
